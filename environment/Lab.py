2ff1fa33d76e492c648cd6298c7fa86407cc0512
hafidgalih <hafidgalih@orbitfutureacademy.sch.id>
2023-05-11 18:44:15 +0700

    Extracted Files

---
+++ environment/Lab.py
@@ -0,0 +1,33 @@
+"""
+Your module description
+"""
+myFruitList = ["apple", 2, "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList.append("orange")
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", 55, "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+mySet = {1, "random"}
+print(mySet)
+print(type(mySet))
+
+myFavoriteFruitDictionary = {
+  "Akua" : ["apple", 2, 2],
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
